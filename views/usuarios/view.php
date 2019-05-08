f057f576a086b59a551903c8eba28e941d01a9a0
José Millán Fernández <jose.millan@iesdonana.org>
2019-05-08 10:59:04 +0200

    Cambiando estilo de los botones de actualizar y borrar perfil

--- views/usuarios/view.php
+++ views/usuarios/view.php
@@ -11,22 +11,43 @@ $this->params['breadcrumbs'][] = ['label' => 'Usuarios', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
+<style>
+  .titulo{
+    float: left;
+  }
+
+  .opciones{
+    float: right;
+  }
+</style>
+
 <div class="usuarios-view">
 
-    <h1><?= Html::encode($model->nombre) ?></h1>
+    <div class="titulo">
+      <h1><?= Html::encode($model->nombre) ?></h1>
+    </div>
 
-    <?php if(Yii::$app->user->id === 1 || Yii::$app->user->id === $model->id ) { ?>
-      <p>
-        <?= Html::a('Actualizar perfil', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Borrar perfil', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-      </p>
-    <?php } ?>
+    <div class="opciones">
+      <span class="dropdown">
+        <button class="glyphicon glyphicon-cog" type="button" data-toggle="dropdown"></button>
+        <ul class="dropdown-menu pull-right">
+          <?php if(Yii::$app->user->id === 1 || Yii::$app->user->id === $model->id ) { ?>
+            <li>
+              <?= Html::a('Modificar perfil', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            </li>
+            <li>
+              <?= Html::a('Borrar perfil', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                  'confirm' => 'Are you sure you want to delete this item?',
+                  'method' => 'post',
+                ],
+                ]) ?>
+              </li>
+            <?php } ?>
+          </ul>
+        </span>
+    </div>
 
     <?= DetailView::widget([
         'model' => $model,

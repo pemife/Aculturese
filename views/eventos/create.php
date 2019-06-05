c32dcc454fae353d80cf1f9f5dfc6a36645c47ad
José Millán Fernández <jose.millan@iesdonana.org>
2019-06-05 19:20:12 +0200

    Refactorizacion de formularios

--- views/eventos/create.php
+++ views/eventos/create.php
@@ -1,113 +1,23 @@
 <?php
 
-use kartik\datetime\DateTimePicker;
-
-use yii\bootstrap\Modal;
-
 use yii\helpers\Html;
-use yii\helpers\Url;
-use kartik\select2\Select2;
-use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Eventos */
 
-$this->title = 'Crear un evento';
+$this->title = 'Crear evento';
 $this->params['breadcrumbs'][] = ['label' => 'Eventos', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
-
-$js = <<<EOF
-$(document).ready(function() {
-  //$('#modalContenido').load($('#modalButton').attr('value'));
-});
-$('#modalButton').click(function(e){
-  $('#modal').modal('show').find($('#modalContenido').load($(this).attr('value')));
-});
-
-EOF;
-$this->registerJs($js);
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Crear';
 ?>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"/>
-<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script>
-
 <div class="eventos-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'inicio')->widget(DateTimePicker::classname(), [
-      'name' => 'Inicio',
+    <?= $this->render('_form', [
+      'listaCategorias' => $listaCategorias,
+      'listaLugares' => $listaLugares,
       'model' => $model,
-      'attribute' => $model->inicio
-    ]); ?>
-
-    <?= $form->field($model, 'fin')->widget(DateTimePicker::classname(), [
-      'name' => 'Fin',
-      'model' => $model,
-      'attribute' => $model->fin
-    ]); ?>
-
-    <?= $form->field($model, 'lugar_id')->widget(Select2::classname(), [
-        'data' => $listaLugares,
-        'options' => ['placeholder' => 'Selecciona un lugar...'],
-        'pluginOptions' => [
-           'allowClear' => true,
-        ],
-      ]);
-    ?>
-
-    <?= Html::button('Lugar nuevo', ['value' => Url::to(['/lugares/create']), 'id' => 'modalButton'], [
-      'class' => 'btn btn-primary',
-      ]) ?>
-
-    <?php
-
-      Modal::begin([
-        'header' => '<h2>Crear lugar</h2>',
-        'id' => 'modal',
-        'size' => 'modal-lg',
-      ]);
-
-      ?>
-      <div id="modalContenido">
-      </div>
-      <?php
-
-      Modal::end();
-
-    ?>
-    <!-- TODO: Quiero conseguir que se pueda crear con un modal donde aparezcan
-    los Lugares señalados con marcas en maps, y que permita añadir una marca
-    nueva para un lugar nuevo creado -->
-
-    <br><br>
-
-    <?= $form->field($model, 'categoria_id')->widget(Select2::classname(), [
-        'data' => $listaCategorias,
-        'options' => ['placeholder' => 'Selecciona una categoria...'],
-        'pluginOptions' => [
-            'allowClear' => true,
-        ],
-      ]);
-    ?>
-
-    <?= $form->field($model, 'creador_id')->hiddenInput([
-            'readonly' => true,
-            'value' => Yii::$app->user->identity->id,
-    ])->label(false);
-    ?>
-
-    <?= $form->field($model, 'imagen')->fileInput() ?>
-
-    <?= $form->field($model, 'es_privado')->dropDownList([false => 'Publico', true => 'Privado'])->label('Privacidad') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
+    ]) ?>
 
 </div>
--- views/eventos/update.php
+++ views/eventos/update.php
@@ -5,17 +5,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Eventos */
 
-$this->title = 'Update Eventos: ' . $model->id;
+$this->title = 'Modificar evento "' . $model->nombre . '"';
 $this->params['breadcrumbs'][] = ['label' => 'Eventos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Actualizar';
 ?>
 <div class="eventos-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
-        'model' => $model,
+      'listaCategorias' => $listaCategorias,
+      'listaLugares' => $listaLugares,
+      'model' => $model,
     ]) ?>
 
 </div>
